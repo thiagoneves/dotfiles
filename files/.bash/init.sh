c53a24d1c6ac0fdebc5405c0d45711924a177d57
thiagoneves <thiagohneves@gmail.com>
2014-07-21 11:29:55 -0300

    add path.sh to init file

--- files/.bash/init.sh
+++ files/.bash/init.sh
@@ -4,5 +4,7 @@ source $HOME/.bash/cd.sh
 source $HOME/.bash/functions.sh
 source $HOME/.bash/general.sh
 source $HOME/.bash/git.sh
+source $HOME/.bash/other.sh
+source $HOME/.bash/path.sh
 source $HOME/.bash/vagrant.sh
 source $HOME/.bash/prompt.sh
--- files/.bash/path.sh
+++ files/.bash/path.sh
@@ -1,1 +1,2 @@
+export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH="/usr/local/bin:$JAVA_HOME/bin:$PATH"

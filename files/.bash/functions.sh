ab38b1deadd8db40de5cdc7963378d6e05a4f964
thiagoneves <thiagohneves@gmail.com>
2014-07-21 11:30:20 -0300

    add gzip function

--- files/.bash/functions.sh
+++ files/.bash/functions.sh
@@ -40,3 +40,22 @@ function fs() {
 		du $arg .[^.]* *;
 	fi;
 }
+
+# Check if given url is giving gzipped content
+function gzipped() {
+  local r=`curl -L --write-out "%{size_download}" --output /dev/null --silent $1`
+  local g=`curl -L -H "Accept-Encoding: gzip,deflate" --write-out "%{size_download}" --output /dev/null --silent $1`
+  local message
+
+  local rs=`expr ${r} / 1024`
+  local gs=`expr ${g} / 1024`
+
+  if [[ "$r" =  "$g" ]]; then
+    message="Regular: ${rs}KB\n\033[31m → Gzip: ${gs}KB\033[0m"
+  else
+    message="Regular: ${rs}KB\n\033[32m → Gzip: ${gs}KB\033[0m"
+  fi
+
+  echo -e $message
+  return 0
+}

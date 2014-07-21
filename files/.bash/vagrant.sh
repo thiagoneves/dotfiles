f4a36c3cf8e3fa3784da995a6197e63b88b3fce7
thiagoneves <thiagohneves@gmail.com>
2014-07-21 16:20:05 -0300

    Add vagrant alias

--- files/.bash/vagrant.sh
+++ files/.bash/vagrant.sh
@@ -1,2 +1,7 @@
+alias vs="vagrant ssh"
+alias vu="vagrant up"
+alias vd="vagrant halt"
+alias vp="vagrant provision"
+
 export VAGRANT_VMWARE_FUSION_APP="/Applications/VMware Fusion.app"
 export VAGRANT_DEFAULT_PROVIDER="vmware_fusion"

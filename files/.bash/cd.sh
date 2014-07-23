c4c8285888e99e8a52b0d8779c52e6bff77a1cde
thiagoneves <thiagohneves@gmail.com>
2014-07-22 21:18:53 -0300

    Open current folder in new window

--- files/.bash/cd.sh
+++ files/.bash/cd.sh
@@ -1,3 +1,13 @@
+export CDHISTORY="/tmp/cd-$USER"
+
+if [ -f $CDHISTORY ]; then
+   dir=$(cat $CDHISTORY)
+
+   if [ -d "$dir" ]; then
+    cd "$dir"
+   fi
+fi
+
 cd() {
-  builtin cd "${@:-$HOME}" && ls;
+  builtin cd "${@:-$HOME}" && ls && pwd > $CDHISTORY;
 }

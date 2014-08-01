871136288a74b5d77f73dfe9298a97a415d1d433
thiagoneves <thiagohneves@gmail.com>
2014-07-31 23:45:35 -0300

    Add brew cask softwares

---
+++ files/.bash/bin/softwares.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# homebrew-cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# browser
+brew cask install firefox
+brew cask install google-chrome
+
+# development
+brew cask install java
+brew cask install sublime-text
+brew cask install filezilla
+brew cask install sourcetree
+brew cask install netbeans
+brew cask install vagrant
+brew cask install vmware-fusion
+
+# photography
+brew cask install adobe-creative-cloud
+
+# other
+brew cask install flash-player
+brew cask install alfred
+brew cask install screenflow
+brew cask install dropbox
+brew cask install google-drive
+brew cask install mou
+brew cask install skype
+brew cask install spotify
+brew cask install transmission
+brew cask install tvshows
+brew cask install trim-enabler

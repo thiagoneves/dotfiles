d46bd2907d62c96ee724be9ad824341234b98ff3
thiagoneves <thiagohneves@gmail.com>
2015-02-20 23:26:39 -0200

    add git completion

--- files/.bash/git.sh
+++ files/.bash/git.sh
@@ -1,3 +1,8 @@
 git config --global user.name "thiagoneves"
 git config --global user.email "thiagohneves@gmail.com"
 git config --global core.excludesfile ~/.gitignore
+
+# git auto-completion
+if [ -f ~/.git-completion ]; then
+  . ~/.git-completion
+fi
